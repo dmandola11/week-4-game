8e74a901563a8c07dec3e65257af55874b42ca59
Damian Mandola Jr <damianjr@Damian-Mandola-Jrs-MacBook-Pro.local>
2016-08-24 13:12:23 -0500

    started html content using bootstrap grid

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+
+//on page load show display random number between 19-120
+
+//assign random numbers to gems between 1-12
+
+// on click of crystal add its value to score
+
+//tally wins/losses
+
+//restart game
