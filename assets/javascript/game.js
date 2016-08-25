3dc54ea6818ac21e17e9bb5106caff0409056045
Damian Mandola Jr <damianjr@Damian-Mandola-Jrs-MacBook-Pro.local>
2016-08-25 16:22:42 -0500

    started game.js- set variables for wins, losses, goal and score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,43 @@
 
+$(document).ready(function() {
 
-//on page load show display random number between 19-120
+//object for buttons and score that will have to be reset every game
+   var startGame = {
+ 	goal: Math.floor(Math.random() * 120) + 19,
+ 	green: Math.floor(Math.random() * 12) + 1,
+ 	purple: Math.floor(Math.random() * 12) + 1,
+ 	yellow: Math.floor(Math.random() * 12) + 1,
+ 	red: Math.floor(Math.random() * 12) + 1,
+ 	score: 0,
+ };
 
-//assign random numbers to gems between 1-12
+console.log(startGame)
+
+//set wins and losses that will keep tally after every game 
+var wins = 0;
+var losses = 0;
+
+//display numbers on page
+ var winSet = $("#wins");
+ var lossSet = $("#loss");
+ var scoreSet = $("#score");
+ var goalSet = $("#goal");
+
+//on page  set random number between 19-120 for buttons and set score to 0
+function initializeGame() {
+		winSet.html(wins);
+		lossSet.html(losses);
+		scoreSet.html(startGame.score);
+		goalSet.html(startGame.goal);
+};
+
+initializeGame();
+//console.log(initializeGame)
 
 // on click of crystal add its value to score
 
 //tally wins/losses
 
 //restart game
+
+})
